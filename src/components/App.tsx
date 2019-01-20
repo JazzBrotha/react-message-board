f367db2b6393621f8f50e0e0a4a1b7b1a4b18ecb
JazzBrotha <mattiasostblom@protonmail.com>
2019-01-20 17:23:28 +0100

    setup basic project structure

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return(
+      <div>
+        <h1>React Message Board App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
